496deb387505166ba388adbfa342955277db0b12
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-21 18:18:49 +0100

    ♻️ extract class

--- src/bio.ts
+++ src/bio.ts
@@ -1,40 +1,54 @@
 #!/usr/bin/env node
 import boxen from "boxen";
 import chalk from "chalk";
+import { DeveloperProfile } from "./developer-profile";
 
-const contactInformation = {
-  name: chalk.bold.green("             Antonio Manuel Díaz Moreno"),
-  work: `${chalk.white('Software developer at')} ${chalk
-    .hex('#2b82b2')
-    .bold('AIDA')}`,
-  github: chalk.gray('https://github.com/') + chalk.green('xenxi'),
+const profile: DeveloperProfile = {
+  name: "Antonio Manuel Díaz Moreno",
+  profession: "Software developer",
+  currentEmployer: "AIDA",
+  githubName: "xenxi",
+  linkedinName: "antonio-manuel-díaz-moreno",
+  websiteUrl: "https://antoniomdm.dev/",
+  consoleCommand: "amdiaz",
+};
+const profileFormated = {
+  name: chalk.bold.green(`             ${profile.name}`),
+  work: `${chalk.white(`${profile.profession} at`)} ${chalk
+    .hex("#2b82b2")
+    .bold("AIDA")}`,
+  github: chalk.gray("https://github.com/") + chalk.green(profile.githubName),
   linkedin:
-    chalk.gray('https://linkedin.com/in/') + chalk.blue('antonio-manuel-díaz-moreno'),
-  web: chalk.cyan('https://antoniomdm.dev/'),
-  npx: `${chalk.red('npx')} ${chalk.white('amdiaz')}`,
+    chalk.gray("https://linkedin.com/in/") + chalk.blue(profile.linkedinName),
+  web: chalk.cyan(profile.websiteUrl),
+  npx: `${chalk.red("npx")} ${chalk.white(profile.consoleCommand)}`,
 
-  labelWork: chalk.white.bold('       Work:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:'),
-}
+  labelWork: chalk.white.bold("       Work:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
 
 const text = [
-  `${contactInformation.name}\n`,
-  `${contactInformation.labelWork}  ${contactInformation.work}\n`,
-  `${contactInformation.labelGitHub}  ${contactInformation.github}`,
-  `${contactInformation.labelLinkedIn}  ${contactInformation.linkedin}`,
-  `${contactInformation.labelWeb}  ${contactInformation.web}\n`,
-  `${contactInformation.labelCard}  ${contactInformation.npx}\n`,
+  `${profileFormated.name}\n`,
+  `${profileFormated.labelWork}  ${profileFormated.work}\n`,
+  `${profileFormated.labelGitHub}  ${profileFormated.github}`,
+  `${profileFormated.labelLinkedIn}  ${profileFormated.linkedin}`,
+  `${profileFormated.labelWeb}  ${profileFormated.web}\n`,
+  `${profileFormated.labelCard}  ${profileFormated.npx}\n`,
   `${chalk.italic(
-    'I would like to become proficient in software development and ensure that I\'m doing it right'
+    "I would like to become proficient in software development and ensure that I'm doing it right"
   )}`,
 ].join("\n");
 
-printWithBoder({ text: text, title: "xenxi" });
+printWithBoder({ text: text, title: "xenxi", profile });
 
-function printWithBoder(params: { text: string; title: string }): void {
+function printWithBoder(params: {
+  text: string;
+  title: string;
+  profile: DeveloperProfile;
+}): void {
   console.log(
     boxen(params.text, {
       title: params.title,
---
+++ src/developer-profile.ts
@@ -0,0 +1,9 @@
+export interface DeveloperProfile {
+  name: string,
+  profession: string,
+  currentEmployer: string,
+  linkedinName: string;
+  githubName: string;
+  websiteUrl: string;
+  consoleCommand: string;
+}
