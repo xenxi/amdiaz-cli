773136ef41aa53a62de1ea015ca24a1e948e7171
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-21 08:33:17 +0100

    📝 update bio

--- src/bio.ts
+++ src/bio.ts
@@ -1,8 +1,34 @@
 import boxen from "boxen";
 import chalk from "chalk";
+
+const contactInformation = {
+  name: chalk.bold.green("             Antonio Manuel Díaz Moreno"),
+  work: `${chalk.white('Software developer at')} ${chalk
+    .hex('#2b82b2')
+    .bold('AIDA')}`,
+  github: chalk.gray('https://github.com/') + chalk.green('xenxi'),
+  linkedin:
+    chalk.gray('https://linkedin.com/in/') + chalk.blue('antonio-manuel-díaz-moreno'),
+  web: chalk.cyan('https://antoniomdm.dev/'),
+  npx: `${chalk.red('npx')} ${chalk.white('amdiaz')}`,
+
+  labelWork: chalk.white.bold('       Work:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:'),
+}
+
 const text = [
-  chalk.bold.green("Antonio Manuel Díaz Moreno"),
-  `${chalk.white.bold("       Work:")}`,
+  `${contactInformation.name}\n`,
+  `${contactInformation.labelWork}  ${contactInformation.work}\n`,
+  `${contactInformation.labelGitHub}  ${contactInformation.github}`,
+  `${contactInformation.labelLinkedIn}  ${contactInformation.linkedin}`,
+  `${contactInformation.labelWeb}  ${contactInformation.web}\n`,
+  `${contactInformation.labelCard}  ${contactInformation.npx}\n`,
+  `${chalk.italic(
+    'I would like to become proficient in software development and ensure that I\'m doing it right'
+  )}`,
 ].join("\n");
 
 printWithBoder({ text: text, title: "xenxi" });
