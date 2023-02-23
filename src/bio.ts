793c851318a82b2fe872331214a9cf1e42dba4e7
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-23 07:42:21 +0100

    ✨ question menu

--- src/bio.ts
+++ src/bio.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { DeveloperProfile } from "./developer-profile.js";
 import { ProfileConsolePresenter } from "./profile-console-presenter.js";
+import inquirer, { Answers, QuestionCollection } from 'inquirer';
 
 console.clear();
 const profile: DeveloperProfile = {
@@ -13,6 +14,35 @@ const profile: DeveloperProfile = {
   consoleCommand: "amdiaz",
 };
 const formattedProfile = ProfileConsolePresenter.print(profile, {
-  title: "amdiaz",
+  title: "👋 Hello world",
 });
 console.log(formattedProfile);
+
+const prompt = inquirer.createPromptModule()
+const choices: Answers[] = [
+  {
+    name: '📧 Send a digital carrier pigeon!',
+    value: () => { console.log('email')},
+  },
+  {
+    name: '📅 Plan a coffee (or tea) talk!',
+    value: () => { console.log('meeting')},
+  },
+  {
+    name: '👀 Take a peek at my work wizardry!',
+    value: () => { console.log('resume')},
+  },
+  {
+    name: '👋 Goodbye, world..',
+    value: () => { console.log('exit')},
+  },
+]
+const questions: QuestionCollection = [
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What\'s your code adventure, adventurer? Choose your destiny! 🧙‍♂️ ',
+    choices,
+  },
+]
+prompt(questions).then(answer => answer.action())
